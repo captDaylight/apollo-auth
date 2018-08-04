f025128643238616c1e4fb086b1cfc21cf2ed7fa
captDaylight <paul.christophe6@gmail.com>
2018-08-04 19:48:30 -0400

    init pass at parcel, WIP not working

---
+++ client/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => (
+  <div>
+    <h1>
+      Hello Parcel
+    </h1>
+  </div>
+);
+
+render(<App />, document.getElementById('app'));
