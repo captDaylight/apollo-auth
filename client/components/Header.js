76e775353e6b08a73204057790b7e03932332f47
captDaylight <paul.christophe6@gmail.com>
2018-09-11 11:13:39 -0400

    switch header on auth

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-import { Mutation } from 'react-apollo';
+import { Mutation, Query } from 'react-apollo';
 import gql from 'graphql-tag';
+import { Link } from 'react-router-dom';
+
+const USER_QUERY = gql`
+  {
+    user {
+      id
+      email
+    }
+  }
+`;
 
 const LOGOUT_MUTATION = gql`
   mutation LogoutMutation {
@@ -14,13 +24,33 @@ const LOGOUT_MUTATION = gql`
 const Header = () => (
   <div className="header">
     header
-    <Mutation
-      mutation={LOGOUT_MUTATION}
-    >
-      {
-        mutation => <button type="submit" onClick={mutation}>Logout</button>
-      }
-    </Mutation>
+
+    <Query query={USER_QUERY}>
+      {({ loading, error, data }) => {
+        if (loading) return <div>Fetching</div>;
+        if (error) {
+          return (
+            <div>
+              <Link to="signup">Signup</Link>
+              <Link to="login">Login</Link>
+            </div>
+          );
+        }
+
+        return (
+          <div>
+            {data.user.email}
+            <Mutation
+              mutation={LOGOUT_MUTATION}
+            >
+              {
+                mutation => <button type="submit" onClick={mutation}>Logout</button>
+              }
+            </Mutation>
+          </div>
+        );
+      }}
+    </Query>
 
   </div>
 );
